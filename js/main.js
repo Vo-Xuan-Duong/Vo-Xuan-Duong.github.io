3effb251d860c3b554d0b63fe6043801f51457cb
Võ Xuân Dương <xuanduong.261104@gmail.com>
2024-08-15 13:19:08 +0700

    Update web Login

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+function OnClickBackground(img) {
+    document.body.style.backgroundImage = `url('${img.src}')`;
+    document.body.style.height = '138vh';
+    document.body.style.backgroundRepeat = 'no-repeat';
+  }
+  
